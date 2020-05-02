06fca0dc0a96ced7c4b9cdb74f5c08cd4ba9d93b
Amruta Abhyankar <amrabh@amrabhair.lan>
2020-05-02 17:20:58 -0400

    url changes

--- cs631/src/app/registration.service.ts
+++ cs631/src/app/registration.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
-const baseUrl = 'http://localhost:4200/api/registration';
+const baseUrl = 'http://localhost:4200/registration';
 
 @Injectable({
   providedIn: 'root'
